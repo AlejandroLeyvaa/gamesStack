dc1891cde784996b025cc8372b1a44c00c34395d
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2021-05-12 21:39:46 -0600

    Some styles fixes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  clientIdPaypal: String(process.env.CLIENT_ID),
-  apiURL: String(process.env.API_URL),
+  clientIdPaypal: process.env.CLIENT_ID,
+  apiURL: process.env.API_URL,
 }
 
 export default config;
